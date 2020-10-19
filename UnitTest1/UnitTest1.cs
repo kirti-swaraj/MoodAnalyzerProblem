f5bb2c59e144395a030a875527ffbe81a2169db8
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-19 07:54:56 +0530

    [kirti] Add . Create mood analyser object using default constructer using reflection

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -7,15 +7,18 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoodAnalyzerProblem;
+using System;
 
 namespace MSTestMoodAnalyzer
 {
     [TestClass]
     public class UnitTest1
     {
-        //UC1
+        /// <summary>
+        /// UC 1.1 : Given the sad mood should return sad
+        /// </summary>
         [TestMethod]
-        public void SadMoodTest()
+        public void GivenSad_ShouldReturnSad()
         {
             //Arrange
             MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
@@ -25,9 +28,12 @@ namespace MSTestMoodAnalyzer
             //Assert
             Assert.AreEqual(expected, actual);
         }
+        /// <summary>
+        /// UC 1.2 : Given the happy mood should return happy
+        /// </summary>
 
         [TestMethod]
-        public void HappyMoodTest()
+        public void GivenHappy_ShouldReturnHappy()
         {
             //Arrange
             MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
@@ -37,9 +43,12 @@ namespace MSTestMoodAnalyzer
             //Assert
             Assert.AreEqual(expected, actual);
         }
+        /// <summary>
+        /// UC 3.1 : Given null should throw moodAnalysis custom exception
+        /// </summary>
 
         [TestMethod]
-        public void GivenNullUsingCustomException()
+        public void GivenNull_ThrowCustomException()
         {
             try
             {
@@ -54,10 +63,12 @@ namespace MSTestMoodAnalyzer
                 Assert.AreEqual("Mood should not be null", exception.Message);
             }
         }
-    
-        //UC2
+
+        /// <summary>
+        /// UC 3.2 : Given empty should throw moodAnalysis custom exception indicating empty mood
+        /// </summary>
         [TestMethod]
-        public void GivenEmptyUsingCustomException()
+        public void GivenEmpty_ThrowCustomException()
         {
             //Arrange
             MoodAnalyser moodAnalyser = new MoodAnalyser("");
@@ -66,6 +77,55 @@ namespace MSTestMoodAnalyzer
             string actual = moodAnalyser.AnalyseMood();
             //Assert
             Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// UC 4.1 : Given moodAnalyser class name should create moodAnalyser object with default constructor
+        /// </summary>
+        [TestMethod]
+        public void GivenMoodAnalyserClassName_ShouldReturnMoodAnalyserObject()
+        {
+            //Arrange
+            var expected = new MoodAnalyser();
+            //Act
+            object result = MoodAnalyserFactory.CreateMoodAnalyserObject("MoodAnalyzerProblem.MoodAnalyser", "MoodAnalyser");
+            //Assert
+            expected.Equals(result);
+        }
+
+        /// <summary>
+        /// UC 4.2 : Given an improper class name should throw mood analysis exception.
+        /// </summary>
+        [TestMethod]
+        public void GivenImproperClassName_ShouldThrowMoodAnalysisException()
+        {
+            try
+            {
+                //Act
+                object result = MoodAnalyserFactory.CreateMoodAnalyserObject("MoodAnalyzerProblemDifferent.MoodAnalyser", "MoodAnalyser");
+            }
+            catch (MoodAnalyserCustomException exception)
+            {
+                //Assert
+                Assert.AreEqual("Exception: class not found", exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// UC 4.3 : Given an improper constructor name should throw mood analysis exception.
+        /// </summary>
+        [TestMethod]
+        public void GivenImproperConstructorName_ShouldThrowMoodAnalysisException()
+        {
+            try
+            {
+                //Act
+                object result = MoodAnalyserFactory.CreateMoodAnalyserObject("MoodAnalyzerProblem.MoodAnalyser", "MoodAnalyserDifferent");
+            }
+            catch (MoodAnalyserCustomException exception)
+            {
+                //Assert
+                Assert.AreEqual("Exception: constructor not found in the class", exception.Message);
+            }
         }
     }
 }
